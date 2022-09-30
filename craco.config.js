f494956b3bb532896735fcb1d61dd09576e7254b
akswnd98 <akswnd98@gmail.com>
2022-09-30 16:35:07 +0900

    fix: issue 반영
    - craco.config.js prettier 반영
    - @emotion/babel-preset-css-prop devDependencies로 이동

--- craco.config.js
+++ craco.config.js
@@ -6,7 +6,7 @@ module.exports = {
       '@src/*': path.resolve(__dirname, 'src'),
       '@src/': path.resolve(__dirname, 'src'),
       '@assets/*': path.resolve(__dirname, 'src/assets'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@assets/': path.resolve(__dirname, 'src/assets'),
       '@icons/*': path.resolve(__dirname, 'src/assets/icons'),
       '@icons/': path.resolve(__dirname, 'src/assets/icons'),
       '@svgs/*': path.resolve(__dirname, 'src/assets/svgs'),
@@ -16,8 +16,6 @@ module.exports = {
     },
   },
   babel: {
-    presets: [
-      '@emotion/babel-preset-css-prop',
-    ],
+    presets: ['@emotion/babel-preset-css-prop'],
   },
 };
