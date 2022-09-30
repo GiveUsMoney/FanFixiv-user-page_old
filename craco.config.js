ea66d9cc1f92f1e888c64ec3af748aab9319a75b
akswnd98 <akswnd98@gmail.com>
2022-09-30 14:57:46 +0900

    feat: craco 관련 preset
    - path alias 설정 (src, src/assets, src/assets/icons, src/assets/svgs, src/assets/theme, * 이상하게 @theme만 잘 안됩니다..))
    - typeRoots 설정 (types에 했습니다.)
    - /** @jsxImportSource @emotion/react */ 자동 추가 (@emotion/react/css 사용 시 매번 선언해 줘야 하는 문제)

---
+++ craco.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@src/*': path.resolve(__dirname, 'src'),
+      '@src/': path.resolve(__dirname, 'src'),
+      '@assets/*': path.resolve(__dirname, 'src/assets'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@icons/*': path.resolve(__dirname, 'src/assets/icons'),
+      '@icons/': path.resolve(__dirname, 'src/assets/icons'),
+      '@svgs/*': path.resolve(__dirname, 'src/assets/svgs'),
+      '@svgs/': path.resolve(__dirname, 'src/assets/svgs'),
+      '@theme/*': path.resolve(__dirname, 'src/assets/theme'),
+      '@theme/': path.resolve(__dirname, 'src/assets/theme'),
+    },
+  },
+  babel: {
+    presets: [
+      '@emotion/babel-preset-css-prop',
+    ],
+  },
+};
