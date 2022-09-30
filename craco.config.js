5d2b56acf284b7b09744b15a567f552b508cceec
akswnd98 <akswnd98@gmail.com>
2022-09-30 21:28:14 +0900

    fix: path alias 안되는 문제
    @theme만 안되는게 아니었습니다.

--- craco.config.js
+++ craco.config.js
@@ -3,16 +3,11 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      '@src/*': path.resolve(__dirname, 'src'),
-      '@src/': path.resolve(__dirname, 'src'),
-      '@assets/*': path.resolve(__dirname, 'src/assets'),
-      '@assets/': path.resolve(__dirname, 'src/assets'),
-      '@icons/*': path.resolve(__dirname, 'src/assets/icons'),
-      '@icons/': path.resolve(__dirname, 'src/assets/icons'),
-      '@svgs/*': path.resolve(__dirname, 'src/assets/svgs'),
-      '@svgs/': path.resolve(__dirname, 'src/assets/svgs'),
-      '@theme/*': path.resolve(__dirname, 'src/assets/theme'),
-      '@theme/': path.resolve(__dirname, 'src/assets/theme'),
+      '@src': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons'),
+      '@svgs': path.resolve(__dirname, 'src/assets/svgs'),
+      '@theme': path.resolve(__dirname, 'src/assets/theme'),
     },
   },
   babel: {
